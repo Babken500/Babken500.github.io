525c1875bdd54c04d0b6b44157b771217aa822c4
Babken500 <babkenzakaryan@gmail.com>
2023-09-05 13:23:56 +0400

    2-d commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from "react";
+import "./styles/style.css";
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/Button/MyButton";
+import { usePosts } from "./hooks/usePosts";
+import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
+import { getPageCount,getPagesArray } from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
+import Posts from "./pages/Posts";
 
-function App() {
+
+const App = function () {
+  const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages]=useState(0);
+  const [limit,setLimit]=useState(10);
+  const [page,setPage]=useState(1);
+  const sortedAndSearchedPosts = usePosts(posts, filter.query.sort, filter.query,);  
+  
+  const [fetchPosts,isPostsLoading,postError]=useFetching(async(limit,page)=>{
+      const response = await PostService.getAll(limit,page);
+      setPosts(response.data);
+      const totalCount=response.headers['x-total-count'];
+      setTotalPages(getPageCount(totalCount,limit));
+  });
+
+  useEffect(() => {
+    fetchPosts(limit,page); 
+  }, []);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+    setModal(false);
+  };
+ 
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const changePage=(page)=>{
+    setPage(page)
+    fetchPosts(limit,page)
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+      <MyButton onClick={() => setModal(true)} style={{ marginTop: 20 }}>Add Post</MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+      <PostFilter filter={filter} setFilter={setFilter} />
+      {postError &&
+        <h1>Error ${postError}</h1>
+      }
+      {isPostsLoading
+        ? <div style={{display:"flex", justifyContent:"center",marginTop:50}}><Loader/></div>
+        :<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Post board"/>
+      }
+      <Pagination page={page} changePage={changePage} totalPages={totalPages}/>
+      <Posts/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import MyInput from "./UI/Input/MyInput";
+import MyButton from "./UI/Button/MyButton";
+
+const PostForm = ({create}) => {
+    const[post,setPost]=useState({title:"",body:""})
+
+    const addNewPost=(e)=>{
+        e.preventDefault()
+        const newPost={
+            ...post,id:Date.now()
+        }
+        create(newPost)
+        setPost({title:"",body:""})
+    }
+
+    return (
+        <form>
+            <MyInput
+                onChange={e=>setPost({...post,title: e.target.value})}
+                value={post.title}
+                type="text"
+                placeholder="Post Name"
+            />
+            <MyInput
+                onChange={e=>setPost({...post,body:e.target.value})}
+                value={post.body}
+                type="text"
+                placeholder="Description"
+            />
+            <MyButton onClick={addNewPost} >Add Post</MyButton>
+
+        </form>
+    );
+};
+
+export default PostForm;
